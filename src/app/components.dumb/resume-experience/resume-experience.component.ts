c82a00c1d4e8ba5849eb63e5833712c1b5d08806
willfynch <william.finzy@protonmail.com>
2024-04-26 15:16:03 +0200

    Fix responsiveness

--- src/app/components.dumb/resume-experience/resume-experience.component.ts
+++ src/app/components.dumb/resume-experience/resume-experience.component.ts
@@ -13,7 +13,7 @@ import { TagModule } from 'primeng/tag';
 })
 export class ResumeExperienceComponent {
 
-  responsiveOptions: any[] | undefined;
+
 
   experiences:Experience[] = [
      {
@@ -69,23 +69,6 @@ export class ResumeExperienceComponent {
   ]
 
   ngOnInit(){
-    this.responsiveOptions = [
-      {
-          breakpoint: '1400px',
-          numVisible: 2,
-          numScroll: 3
-      },
-      {
-          breakpoint: '1220px',
-          numVisible: 2,
-          numScroll: 2
-      },
-      {
-          breakpoint: '1100px',
-          numVisible: 1,
-          numScroll: 1
-      }
-    ];
   }
   
 }
