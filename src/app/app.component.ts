8036793e8f077e06e98d94ea4b870ec5e9bbe1e5
willfynch <william.finzy@protonmail.com>
2024-05-23 19:05:30 +0200

    footer / wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { Observable, Subscription, catchError, mergeMap, of } from 'rxjs';
 import { NavbarComponent } from './components.dumb/layout/navbar/navbar.component';
+import { FooterComponent } from './components.dumb/layout/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components.dumb/layout/footer/footer.component.ts
+++ src/app/components.dumb/layout/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -8,5 +9,18 @@ import { Component } from '@angular/core';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
+  currentRoute: string | undefined = '';
+  constructor(private router: Router){
+
+  }
+  ngOnInit(){
+    this.router.events.subscribe((event:any)=>{
+      if(event instanceof NavigationEnd){
+       this.currentRoute = event.urlAfterRedirects
+      }
+   })
+  }
+
 
 }
