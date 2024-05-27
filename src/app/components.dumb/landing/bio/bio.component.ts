6403447e258f7c61f5a8b27599fad0a46d8bf5cc
willfynch <william.finzy@protonmail.com>
2024-05-27 14:42:31 +0200

    add ssr / wip

--- src/app/components.dumb/landing/bio/bio.component.ts
+++ src/app/components.dumb/landing/bio/bio.component.ts
@@ -11,6 +11,14 @@ import {MatTooltipModule} from '@angular/material/tooltip';
 })
 export class BioComponent {
 
+  bio = `
+  Bonjour ! Je suis un développeur Angular et NextJS avec 3 ans d'expérience. Je me suis formé d'abord 
+  en autodidacte puis j'ai rejoint une formation par alternance d'un an au sein de la PMN avec un apprentissage chez
+  Capgemini. 
+  Je suis en mission depuis 2 ans sur un poste de développeur front-end Angular. À côté, je produis des sites pour solopreneurs
+  et je documente sur LinkedIn la création de mon premier SaaS.
+  `
+
   technos: Techno[] = [
 
     {name: 'Angular', image: '../../../assets/svgs/logo_angular.svg'},
