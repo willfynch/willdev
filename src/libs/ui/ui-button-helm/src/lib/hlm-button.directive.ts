ecc1d92a7b498844ce8c0a92dc1ca313fd9e8be2
willfynch <william.finzy@protonmail.com>
2025-02-26 13:21:42 +0100

    Test with bigger font

--- src/libs/ui/ui-button-helm/src/lib/hlm-button.directive.ts
+++ src/libs/ui/ui-button-helm/src/lib/hlm-button.directive.ts
@@ -17,7 +17,7 @@ export const buttonVariants = cva(
                     'border border-input hover:bg-accent hover:text-accent-foreground',
                 secondary:
                     'bg-secondary text-secondary-foreground hover:bg-secondary/80',
-                ghost: 'hover:bg-accent hover:text-accent-foreground',
+                ghost: 'hover:bg-accent hover:text-accent-foreground text-base',
                 link: 'underline-offset-4 hover:underline text-primary',
                 ghostNoLink: 'hover:bg-accent hover:text-accent-foreground cursor-default',
                 blueBtn: 'bg-accent-blue-light text-accent-blue'
